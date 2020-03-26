73eef33182ca72729f3d2bc626e1a59aacad7066
陳家揚Elian <56495126+jychen0611@users.noreply.github.com>
2020-03-26 21:22:49 +0800

    1.2

--- WHOMAIN.py
+++ WHOMAIN.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import QTableWidget,QFrame,QAbstractItemView, QTableWidgetI
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
-from good2 import Ui_MainWindow
+from WHOUI import Ui_MainWindow
 
 
 import sys
